da9eb62f95075893fe7512499e23958b2ac895ba
dongguk <leedongguk@kakao.com>
2022-05-26 01:44:32 +0900

    로딩, 뷰타입변경, 필터

--- panel.js
+++ panel.js
@@ -4,12 +4,36 @@
 // chrome.devtools.*
 // chrome.extension.*
 
+var isFold = new Boolean();
+if(localStorage.getItem('isFold')){
+    isFold = localStorage.getItem('isFold') === 'true'
+}else{
+    isFold = true;
+    localStorage.setItem('isFold', isFold);
+}
+var lastEventId = 0;
+var events = {};
+var status = "";
+
 window.onload = function () {
     document.querySelector('.clear').addEventListener('click', clearEvents);
+    document.querySelector('.view').addEventListener('click', toggleFold);
     document.querySelector('.search').addEventListener('keyup', filter);
+    changeViewType()
 }
 
-var lastEventId = 0;
+chrome.tabs.onUpdated.addListener( (id, info) => {
+    if(info.status){
+        status = info.status
+    }
+    if(status == "loading"){
+        document.querySelector(".loading").classList.remove("hide");
+        document.querySelector(".event-length").classList.add("hide");
+    }else{
+        document.querySelector(".loading").classList.add("hide");
+        document.querySelector(".event-length").classList.remove("hide");
+    }
+})
 
 chrome.devtools.network.onRequestFinished.addListener(
     function(d) {
@@ -18,6 +42,7 @@ chrome.devtools.network.onRequestFinished.addListener(
                 const data = JSON.parse(decodeURIComponent(d.request.postData.params.filter(i=>i.name == 'e')[0].value))
                 data.filter(i => i.event_type != '$identify').forEach(event => {
                     if(event.event_id > lastEventId){
+                        events[event.event_id] = event.event_properties;
                         drawEvent(event)
                         lastEventId = event.event_id
                         changeEventsLength()
@@ -34,21 +59,26 @@ var drawEvent = function(data){
 
     var EventElem = document.createElement("div");
 	EventElem.classList.add("event");
+    EventElem.addEventListener( "click", drawProps)
+    EventElem.setAttribute('event_id', data.event_id)
     if(data.event_type.toLowerCase().indexOf(searchValue) != -1){
-        EventElem.style.display = "flex";
+        EventElem.classList.remove("hide");
     }else{
-        EventElem.style.display = "none";
+        EventElem.classList.add("hide");
     }
 
     var TitleElem = document.createElement("p");
 	TitleElem.classList.add("title");
     TitleElem.innerText = data.event_type;
-    EventElem.append(TitleElem);
 
     var PropsElem = document.createElement("div");
-	PropsElem.classList.add("props");
-    EventElem.append(PropsElem);
-
+	PropsElem.classList.add("in-props");
+    console.log(isFold)
+    if(isFold){
+        console.log(isFold)
+        PropsElem.classList.add("hide");
+    }
+    
     Object.keys(data.event_properties).forEach( key => {
         var PropElem = document.createElement("div");
         PropElem.classList.add("prop");
@@ -66,38 +96,102 @@ var drawEvent = function(data){
         ValueElem.classList.add("value");
         ValueElem.innerText = data.event_properties[key];
         PropElem.append(ValueElem);
-
         PropsElem.append(PropElem);
     });
 
+    EventElem.append(TitleElem);
+    EventElem.append(PropsElem);
     EventsElem.prepend(EventElem);
 }
 
+var drawProps = function(e){
+    var Elem = e.currentTarget;
+    var id = Elem.getAttribute("event_id");
+    clearProps()
+    
+    Elem.classList.add("selected");
+    var PropsElem = document.querySelector('.props');
+    Object.keys(events[id]).forEach( key => {
+        var PropElem = document.createElement("div");
+        PropElem.classList.add("prop");
+        
+        var KeyElem = document.createElement("p");
+        KeyElem.classList.add("key");
+        KeyElem.innerText = key;
+        PropElem.append(KeyElem);
+    
+        var ColonElem = document.createElement("p");
+        ColonElem.innerText = ":";
+        PropElem.append(ColonElem);
+    
+        var ValueElem = document.createElement("p");
+        ValueElem.classList.add("value");
+        ValueElem.innerText = events[id][key];
+        PropElem.append(ValueElem);
+
+        PropsElem.append(PropElem);
+    });
+}
+
+var clearProps = function(){
+    var PropsElem = document.querySelector('.props');
+    PropsElem.innerText = ''
+    if(document.querySelector(".selected")){
+        document.querySelector(".selected").classList.remove("selected");
+    }
+}
 var clearEvents = function(){
     var EventsElem = document.querySelector('.events');
     EventsElem.innerText = ''
+    clearProps()
+    changeEventsLength()
 }
 var filter = function(){
     var searchValue = document.querySelector('.search').value.toLowerCase();
-    var eventElem = document.querySelectorAll('.event');
+    var EventElem = document.querySelectorAll('.event');
 
-    for(i = 0; i < eventElem.length; i++){
-        title = eventElem[i].getElementsByClassName("title");
+    for(i = 0; i < EventElem.length; i++){
+        title = EventElem[i].getElementsByClassName("title");
         if(title[0].innerText.toLowerCase().indexOf(searchValue) != -1){
-            eventElem[i].style.display = "flex";
+            EventElem[i].classList.remove("hide");
         }else{
-            eventElem[i].style.display = "none";
+            EventElem[i].classList.add("hide");
         }
     }
     changeEventsLength()
+    clearProps()
 }
 var changeEventsLength = function(){
-    var countElem = document.querySelector(".count");
-    var eventElem = document.querySelectorAll('.event');
-    var hideElem = document.querySelectorAll('.event[style="display: none;"]');
-    if(hideElem.length == 0){
-        countElem.innerHTML = eventElem.length;
+    var CountElem = document.querySelector(".count");
+    var EventElem = document.querySelectorAll('.event');
+    var HideElem = document.querySelectorAll('.event.hide');
+    if(HideElem.length == 0){
+        CountElem.innerHTML = EventElem.length;
     }else{
-        countElem.innerHTML = eventElem.length - hideElem.length + "/" + eventElem.length;
+        CountElem.innerHTML = EventElem.length - HideElem.length + "/" + EventElem.length;
+    }
+}
+var toggleFold = function(){
+    localStorage.setItem('isFold', !isFold);
+    isFold = !isFold;
+    changeViewType()
+}
+var changeViewType = function(){
+    var Props = document.querySelectorAll(".in-props");
+    clearProps()
+    if(isFold){
+        document.querySelector(".view").innerText = "expand";
+        document.querySelector(".props").classList.remove("hide");
+        document.querySelector(".events").classList.remove("wide");
+        for (var i = 0; i < Props.length; i++) {
+            Props[i].classList.add("hide")
+        }
+    }else{
+        document.querySelector(".view").innerText = "fold";
+        document.querySelector(".props").classList.add("hide");
+        document.querySelector(".events").classList.add("wide");
+        for (var i = 0; i < Props.length; i++) {
+            Props[i].classList.remove("hide")
+        }
     }
 }
